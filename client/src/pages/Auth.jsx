3924fa0167fd5d08c5d07215195b9c2fe1fcf731
Sema <semaesgerova68@gmail.com>
2023-09-01 01:00:30 +0400

    tailwind

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className='w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 bottom-0 left-0 right-0 z-50'>
+        <div className='w-1/3 bg-white '>
+              <h1>Post Paylas</h1>
+              <div>
+                <input type="text" />
+              </div>
+        </div>
+    </div>
   )
 }
 
